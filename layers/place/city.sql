0daf75cabe682d4f2c88c317cb13207ebb46c27a
marym <marymcguire1718@gmail.com>
2021-02-17 21:48:31 +0000

    showing all places with gridrank less than 100 for zooms below 7

--- layers/place/city.sql
+++ layers/place/city.sql
@@ -31,7 +31,7 @@ FROM (
          FROM osm_city_point
          WHERE geometry && bbox
            AND ((zoom_level = 2 AND "rank" = 1)
-             OR (zoom_level BETWEEN 3 AND 7 AND "rank" <= zoom_level + 1) -- city points with null rank are not selected for zooms below 7
+              OR (zoom_level BETWEEN 3 AND 7 AND "rank" <= zoom_level + 0) -- city points with null rank are not selected for zooms below 7
              )
          UNION ALL
          SELECT osm_id,
@@ -41,7 +41,7 @@ FROM (
                 COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
                 tags,
                 place,
-                COALESCE("rank", gridrank + 10),
+                COALESCE("rank", gridrank + 10), -- 
                 normalize_capital_level(capital) AS capital
          FROM (
                   SELECT osm_id,
@@ -62,13 +62,13 @@ FROM (
                              )::int AS gridrank
                   FROM osm_city_point
                   WHERE geometry && bbox
-                    AND ((zoom_level = 7 AND place <= 'town'::city_place
+                    AND ((zoom_level <= 7 AND place <= 'town'::city_place
                       OR (zoom_level BETWEEN 8 AND 10 AND place <= 'village'::city_place)
                       OR (zoom_level BETWEEN 11 AND 13 AND place <= 'suburb'::city_place)
                       OR (zoom_level >= 14)
                       ))
               ) AS ranked_places
-         WHERE (zoom_level BETWEEN 7 AND 8 AND (gridrank <= 4 OR "rank" IS NOT NULL))
+         WHERE (zoom_level BETWEEN 3 AND 8 AND (gridrank <= 100 OR "rank" IS NOT NULL))
             OR (zoom_level = 9 AND (gridrank <= 8 OR "rank" IS NOT NULL))
             OR (zoom_level = 10 AND (gridrank <= 12 OR "rank" IS NOT NULL))
             OR (zoom_level BETWEEN 11 AND 12 AND (gridrank <= 14 OR "rank" IS NOT NULL))
