9f7cb4c7e994bc4a8b1bb57ec6103a2182af2c00
marym <marymcguire1718@gmail.com>
2021-02-19 19:15:43 +0000

    adjusted rank filter

--- layers/place/city.sql
+++ layers/place/city.sql
@@ -68,10 +68,10 @@ FROM (
                       OR (zoom_level >= 14)
                       ))
               ) AS ranked_places
-         WHERE (zoom_level BETWEEN 3 AND 8 AND (gridrank <= 50 OR "rank" IS NOT NULL))
-            OR (zoom_level = 9 AND (gridrank <= 100 OR "rank" IS NOT NULL))
-            OR (zoom_level = 10 AND (gridrank <= 150 OR "rank" IS NOT NULL))
-            OR (zoom_level BETWEEN 11 AND 12 AND (gridrank <= 200 OR "rank" IS NOT NULL))
+         WHERE (zoom_level BETWEEN 3 AND 8 AND (gridrank <= 15 OR "rank" IS NOT NULL))
+            OR (zoom_level = 9 AND (gridrank <= 30 OR "rank" IS NOT NULL))
+            OR (zoom_level = 10 AND (gridrank <= 45 OR "rank" IS NOT NULL))
+            OR (zoom_level BETWEEN 11 AND 12 AND (gridrank <= 60 OR "rank" IS NOT NULL))
             OR (zoom_level >= 13)
      ) AS city_all;
 $$ LANGUAGE SQL STABLE
